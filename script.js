951123d0b6aabadb9ee9f77826a46fdf9d22081a
Your Name <hossainhridoy@30687@gmail.com>
2022-08-28 14:21:04 +0600

    finish

--- script.js
+++ script.js
@@ -2,8 +2,21 @@ const menuBar = document.getElementById('menu-bar')
 const navCenter = document.getElementById('nav-center')
 const ourStore = document.getElementById('our-store')
 const storeCardContainer = document.getElementById('store-card-container')
+const input = document.getElementById('input')
+const allBtn = document.getElementById('all-btn')
+const cakeBtn = document.getElementById('cake-btn')
+const cupcakeBtn = document.getElementById('cupcake-btn')
+const sweetBtn = document.getElementById('sweet-btn')
+const doughnutBtn = document.getElementById('doughnut-btn')
 
 menuBar.addEventListener('click', menuDownShow)
+input.addEventListener('keyup', (e) => foodFn(e.target.value))
+allBtn.addEventListener('click', () => foodFn(''))
+cakeBtn.addEventListener('click', () => foodFn2('Cake Item'))
+cupcakeBtn.addEventListener('click', () => foodFn('cupcake item'))
+sweetBtn.addEventListener('click', () => foodFn('sweet item'))
+doughnutBtn.addEventListener('click', () => foodFn('dougnut item'))
+
 let menu = false;
 function menuDownShow() {
     if(!menu){
@@ -15,24 +28,23 @@ function menuDownShow() {
     }
 }
 
-let search = null;
-const foodFn = () => {
+const foodFn = (e) => {
+    let search = e || '';
     fetch('foods.json')
     .then(res => res.json())
     .then(data => {
-        console.log(data)
-        // data.map(s => div.innerHTML = `${s.name}`)
-        // storeCardContainer.appendChild(div)
-        for(const d of data){
+        storeCardContainer.innerHTML = ''
+        const filter = data.filter(d => d.name.toLowerCase().includes(search))
+        for(const f of filter){
             const div = document.createElement('div')
             div.innerHTML = `
                 <div class='store-card-1'>
-                    <img src=${d.img} alt=>
+                    <img src=${f.img} alt=''>
                     <i class="fa-solid fa-cart-shopping"></i>
                 </div>
                 <div class='store-card-2'>
-                    <span>${d.name}</span>
-                    <b>$${d.price}</b>
+                    <span>${f.name}</span>
+                    <b>$${f.price}</b>
                 </div>
             `
             div.classList.add('store-card')
@@ -41,3 +53,27 @@ const foodFn = () => {
     })
 }
 foodFn()
+const foodFn2 = (e) => {
+    let search = e || '';
+    fetch('foods.json')
+    .then(res => res.json())
+    .then(data => {
+        storeCardContainer.innerHTML = ''
+        const filter = data.filter(d => d.name.includes(search))
+        for(const f of filter){
+            const div = document.createElement('div')
+            div.innerHTML = `
+                <div class='store-card-1'>
+                    <img src=${f.img} alt=''>
+                    <i class="fa-solid fa-cart-shopping"></i>
+                </div>
+                <div class='store-card-2'>
+                    <span>${f.name}</span>
+                    <b>$${f.price}</b>
+                </div>
+            `
+            div.classList.add('store-card')
+            storeCardContainer.appendChild(div)
+        }
+    })
+}
